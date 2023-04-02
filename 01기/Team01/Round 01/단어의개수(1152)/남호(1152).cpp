e80929634f038bdc90e8290d3c5eb9acda94df29
1829122 <100738276+1829122@users.noreply.github.com>
2023-04-02 20:59:13 +0900

    Update 남호(1152).cpp

--- 01기/Team01/Round 01/단어의개수(1152)/남호(1152).cpp
+++ 01기/Team01/Round 01/단어의개수(1152)/남호(1152).cpp
@@ -1,1 +1,22 @@
-dsadsadsad
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <map>
+#include <iomanip>
+using namespace std;
+
+
+
+int main() {
+    string s;
+    getline(cin, s); 
+    int i=0,num=0;
+    while(s[i]!='\0'){
+    	if (s[i] != ' ' && (i == 0 || s[i-1] == ' ')){ 
+        	num++;
+        }
+        i++;
+    } 
+    cout << num; 
+}
