56111bfc785702f0291d08d5b648d072be6c1c38
1829122 <100738276+1829122@users.noreply.github.com>
2023-04-02 21:19:19 +0900

    Update 남호(1193).cpp

--- 01기/Team01/Round 01/분수찾기(1193)/남호(1193).cpp
+++ 01기/Team01/Round 01/분수찾기(1193)/남호(1193).cpp
@@ -1,1 +1,29 @@
-ssss
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int n;
+	cin >> n;
+
+	int i = 0;
+
+	while (n > 0) 
+	{
+		i++;
+		n -= i;
+	}
+
+	if (i % 2 == 1) 
+	{
+		cout << 1 - n << "/" << i + n << "\n";
+	}
+	else if (i % 2 == 0) 
+	{
+		cout << i + n << "/" << 1 - n << "\n";
+	}
+}
